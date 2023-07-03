70a79436789dbf6ee53aff1e93767470380eb406
rahul_bhataniya <rahulbhataniya2468@gmail.com>
2023-07-03 23:23:40 +0530

    Use Get And Post Methods

---
+++ src/people.js
@@ -0,0 +1,19 @@
+export let people=[{
+  name : "Deepak",
+  age  : 25,
+  haircolor: "red"
+},
+{
+name : "Nirmal",
+age :25,
+haircolor :"white"
+},
+{
+name :"Hariom",
+age:30,
+haircolor: "black"
+},
+{name :"ramgopal",
+age :30,
+haircolor: "brown"}
+]
